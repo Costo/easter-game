70df5663239dcbb97e73bef7d078259f6d068be0
Vincent Costel <vcostel@thinkwellgroup.com>
2021-02-06 22:29:44 -0500

    Add guard dialog

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -1,12 +1,29 @@
 extends KinematicBody2D
 
 const EnemyDeathEffect = preload("res://Effects/EnemyDeathEffect.tscn")
+
+enum {
+	IDLE,
+	WANDER,
+	CHASE
+}
+
 onready var stats = $Stats
 var knockback = Vector2.ZERO
+var state = IDLE
 
 func _physics_process(delta):
 	knockback = knockback.linear_interpolate(Vector2.ZERO, 0.1)
 	knockback = move_and_slide(knockback)
+	
+	match state:
+		IDLE:
+			pass
+		WANDER:
+			pass
+		CHASE: 
+			pass
+		
 	
 func _on_HurtBox_area_entered(area):
 	stats.health -= area.damage
